f4ec5326980208b4aa15be9328e4cd85d844f0ca
Jeffrey Emakpor <jeffreyefemena4@gmail.com>
2020-11-26 10:05:04 +0100

    Update config-overrides.js

--- projects/core-app/config-overrides.js
+++ projects/core-app/config-overrides.js
@@ -1,47 +1,82 @@
 const {
-  override,
-  addBabelPlugin,
-  getBabelLoader,
-  addWebpackPlugin,
-  removeModuleScopePlugin,
-  addWebpackModuleRule,
-} = require("customize-cra");
-const ReactRefreshPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
-const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+    override,
+    addBabelPlugin,
+    getBabelLoader,
+    addWebpackPlugin,
+    addWebpackModuleRule,
+    addWebpackResolve,
+} = require("customize-cra")
+const path = require("path")
+const WorkerPlugin = require("worker-plugin")
 
-const dev = process.env.REACT_APP_NODE_ENV === "development";
+// const {addReactRefresh} = require("customize-cra-react-refresh")
+const ReactRefreshPlugin = require("@pmmmwh/react-refresh-webpack-plugin")
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin")
+
+const dev = process.env.REACT_APP_NODE_ENV === "development"
+const monoplatform = process.env.REACT_APP_PLATFORM === "mono"
+
 module.exports = (config, env) => {
-  const babelLoader = getBabelLoader(config);
-  babelLoader.include = babelLoader.include.replace("/web/src", "");
-  config.module.rules[2].oneOf[1] = babelLoader;
+    const babelLoader = getBabelLoader(config)
+    babelLoader.include = babelLoader.include.replace("/web/src", "")
+    config.module.rules[2].oneOf[1] = babelLoader
+    const configFile = () => {
+        if (!monoplatform) return "./tsconfig.json"
+        if (monoplatform) return "./tsconfig.mono.json"
+    }
 
-  return override(
-    addWebpackModuleRule({
-      test: /\.(tsx|ts)$/,
-      use: [
-        {
-          loader: babelLoader.loader,
-          options: babelLoader.options,
-        },
-        // {loader: "awesome-typescript-loader"},
-      ],
-    }),
+    /**
+     *
+     *
+     * remeber to load ts loader before worker
+     */
+    return override(
+        // You can choose to just use worker-loader! instead if you want
+        addWebpackModuleRule({
+            test: /\.worker\.[jt]sx?$/,
+            use: [
+                // {
+                //     loader: "worker-loader",
+                // },
+                {
+                    loader: babelLoader.loader,
+                    options: babelLoader.options,
+                },
+            ],
+        }),
 
-    // You can choose to just use worker-loader! instead if you want
-    addWebpackModuleRule({
-      test: /\.worker\.[jt]sx?$/,
-      use: [
-        { loader: "worker-loader" },
-        { loader: babelLoader.loader, options: babelLoader.options },
-      ],
-    }),
-    removeModuleScopePlugin(),
+        addWebpackModuleRule({
+            test: /\.(tsx|ts)$/,
+            use: [
+                {
+                    loader: babelLoader.loader,
+                    options: babelLoader.options,
+                },
+            ],
+        }),
 
-    dev && addBabelPlugin("react-refresh/babel"),
-    dev &&
-      addWebpackPlugin(
-        new ReactRefreshPlugin({ forceEnable: true }),
-        new TsconfigPathsPlugin()
-      )
-  )(config, env);
-};
+        addWebpackPlugin(new WorkerPlugin()),
+
+        dev &&
+            addWebpackPlugin(
+                new ReactRefreshPlugin({
+                    forceEnable: true,
+                })
+            ),
+        dev && addBabelPlugin("react-refresh/babel"),
+        // dev && addReactRefresh({forceEnable: true, useLegacyWDSSockets: true}),
+        addWebpackResolve({
+            symlinks: true,
+            alias: {
+                vendor: path.resolve(__dirname, "src/"),
+            },
+            plugins: [
+                new TsconfigPathsPlugin({
+                    logLevel: "INFO",
+                    extensions: [".ts", ".tsx", ".js", ".jsx"],
+                    configFile: configFile(),
+                }),
+            ],
+        })
+    )(config, env)
+}
